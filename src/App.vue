d97011d16d15b761a2bdeb91acd987f466917f3a
wcshds <wcshds@foxmail.com>
2023-10-15 12:46:51 +0800

    show the trick on the page

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,15 @@ let text = ref("abcるde😂éàö𣎴ùçてëêîcÏ💕😒測し試");
     <input type="text" v-model="text" placeholder="input some characters" />
 
     <p class="mt-5">output:</p>
-    <p class="mx-5 mt-2 border h-20 rounded">
+    <p class="mx-5">with normal replace method</p>
+    <p class="mx-5 border h-20 rounded">
       {{ text.replace(/[^\u{10000}-\u{10FFFF}]/gu, "") }}
-
-      <!-- It works, but it doesn't look intuitive. -->
-      <!-- {{ Array.from(text.matchAll(/[\u{10000}-\u{10FFFF}]/ug)).join("") }} -->
     </p>
-
+    <p class="mx-5 mt-3">call matchAll first, then turn the iterator into an array, finally join characters together </p>
+    <p class="mx-5 border h-20 rounded">
+      <!-- It works, but it doesn't look intuitive. -->
+      {{ Array.from(text.matchAll(/[\u{10000}-\u{10FFFF}]/ug)).join("") }}
+    </p>
     <p class="mt-16">Note:</p>
     <p class="ml-5">
       This basic character replacement (implemented using regular expression in
